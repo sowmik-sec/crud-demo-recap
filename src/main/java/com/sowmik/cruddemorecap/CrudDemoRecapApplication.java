5d33714ce17098a43020aa37e4daa5cabdfc9114
sowmik-sec <sowmik.sec@gmail.com>
2023-10-24 06:21:52 +0600

    setting the project

---
+++ src/main/java/com/sowmik/cruddemorecap/CrudDemoRecapApplication.java
@@ -0,0 +1,21 @@
+package com.sowmik.cruddemorecap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CrudDemoRecapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudDemoRecapApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(String[] args) {
+        return runner -> {
+            System.out.println("Hello World");
+        };
+    }
+}
