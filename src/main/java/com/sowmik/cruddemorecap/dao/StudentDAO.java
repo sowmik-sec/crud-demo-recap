4bd024f5086701c951e7eb8889310e64ebaca32e
sowmik-sec <sowmik.sec@gmail.com>
2023-10-24 21:19:42 +0600

    display all objects using query with jpa

--- src/main/java/com/sowmik/cruddemorecap/dao/StudentDAO.java
+++ src/main/java/com/sowmik/cruddemorecap/dao/StudentDAO.java
@@ -2,7 +2,10 @@ package com.sowmik.cruddemorecap.dao;
 
 import com.sowmik.cruddemorecap.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
     void save(Student theStudent);
     Student findById(Integer id);
+    List<Student> findAll();
 }
